0a0f369d368bdc6f24e4cbfcbc39c2facd9366de
Kristóf Kerti <47832952+kkerti@users.noreply.github.com>
2025-09-19 16:37:49 +0200

    kk remove greg comment

--- src/index.ts
+++ src/index.ts
@@ -156,7 +156,6 @@ async function selectionListener() {
           await volume.addListener("value", (v) => {
             activeTrackVolumeValue = v.toFixed(2);
             // If we send right away the set changes, circular triggers are present
-            console.log("GREG", activeTrackVolumeValue);
             // sendActivePropertyToGrid();
           })
         );
